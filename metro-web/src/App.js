659235c6497616a2d7b9b830d21ebff8a84191ed
Andrew Burdiak <Andrew.Burdiak@gmail.com>
2019-10-15 17:52:19 -0400

    Comitting before serverless install

--- metro-web/src/App.js
+++ metro-web/src/App.js
@@ -1,16 +1,20 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => {
-  console.log("hello");
+  console.log('hello');
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+and save to reload.
         </p>
         <a
           className="App-link"
